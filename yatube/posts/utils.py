6c13a7410a59c3f647349ff05ed163cff1ac4592
Дмитрий Спиряев <imwisagist@yandex.ru>
2022-06-11 22:37:31 +0300

    2 ревью 6 спринт

--- yatube/posts/utils.py
+++ yatube/posts/utils.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.core.paginator import Paginator
 
+
 def paginate_posts(request, post_list):
     paginator = Paginator(post_list, settings.POSTS_ON_PAGE)
     page_number = request.GET.get('page')
